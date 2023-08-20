16d6b8afd9a26e014796139906a394c1f36c3dc4
singh-that-codes <500091028@stu.upes.ac.in>
2023-08-21 03:39:35 +0530

    Commiting the process

--- lib/pages/frame/sign_in/view.dart
+++ lib/pages/frame/sign_in/view.dart
@@ -1,67 +1,128 @@
-
 import 'package:chatty/common/values/colors.dart';
 import 'package:chatty/pages/frame/sign_in/controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
-
 class SignInPage extends GetView<SignInController> {
   const SignInPage({super.key});
 
-Widget _buildLogo(){
-  return Container(
-    margin: EdgeInsets.only(top:100.h,bottom:80.h),
-    child: const Text(
-      "Chit-Chat",
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        color: AppColors.primaryText,
-        fontWeight: FontWeight.bold,
-        fontSize: 34
-      )
-    ),
-  );
-}
-Widget _buildThirdPartyLogin(String loginType, String logo){
-  return Container(
-    width: 295.w,
-    height: 44.h,
-    padding: EdgeInsets.all(10.h),
-    margin: EdgeInsets.only(bottom: 15.h),
-    decoration: BoxDecoration(
-      color: AppColors.primaryBackground,
-      borderRadius: const BorderRadius.all(Radius.circular(5)),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.grey.withOpacity(0.1),
-          spreadRadius: 1,
-          blurRadius: 2,
-          offset: const Offset(0, 1),
-        )
-      ]
-    ),
-    child: Row(
-      children: [
-        Container(
-          padding: EdgeInsets.only(left:40.h,right:30.w),
-          child: Image.asset('assets/icons/$logo.png'),
+  Widget _buildLogo() {
+    return Container(
+      margin: EdgeInsets.only(top: 100.h, bottom: 80.h),
+      child: const Text(
+        "Chit-Chat",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: AppColors.primaryText,
+          fontWeight: FontWeight.bold,
+          fontSize: 34,
         ),
-        Container(
-          child: Text(
-            "Sign in with $loginType",
-            style: const TextStyle(
-            color: AppColors.primaryText,
-            fontWeight: FontWeight.bold,
-            fontSize: 14
-            )
-          ), 
+      ),
+    );
+  }
+
+  Widget _buildThirdPartyLogin(String loginType, String logo) {
+    return GestureDetector(
+      onTap: (() {
+        print("Sign Up with third party");
+      }),
+      child: Container(
+        width: 295.w,
+        height: 44.h,
+        padding: EdgeInsets.all(10.h),
+        margin: EdgeInsets.only(bottom: 15.h),
+        decoration: BoxDecoration(
+          color: AppColors.primaryBackground,
+          borderRadius: const BorderRadius.all(Radius.circular(5)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 2,
+              offset: const Offset(0, 1),
+            ),
+          ],
         ),
-      ],
-    )
-  );
-  
-}
+        child: Row(
+          mainAxisAlignment: logo == '' ? MainAxisAlignment.center : MainAxisAlignment.start,
+          children: [
+            logo == ''
+                ? Container()
+                : Container(
+                    padding: EdgeInsets.only(left: 40.h, right: 30.w),
+                    child: Image.asset('assets/icons/$logo.png'),
+                  ),
+            Container(
+              child: Text(
+                "Sign in with $loginType",
+                style: const TextStyle(
+                  color: AppColors.primaryText,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOrWidget() {
+    return Container(
+      margin: EdgeInsets.only(top: 20.h, bottom: 35.h),
+      child: Row(
+        children: [
+          Expanded(
+            child: Divider(
+              indent: 50,
+              height: 2.h,
+              color: AppColors.primarySecondaryElementText,
+            ),
+          ),
+          const Text(" or "),
+          Expanded(
+            child: Divider(
+              endIndent: 50,
+              height: 2.h,
+              color: AppColors.primarySecondaryElementText,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSignUpWidget() {
+    return GestureDetector(
+      onTap: (() {
+        print('SignUp here');
+      }),
+      child: Column(
+        children: [
+          const Text(
+            "Already have an account",
+            style: TextStyle(
+              color: AppColors.primaryText,
+              fontWeight: FontWeight.bold,
+              fontSize: 12,
+            ),
+          ),
+          GestureDetector(
+            child: const Text(
+              "Sign up here",
+              style: TextStyle(
+                color: AppColors.primaryElement,
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,35 +132,18 @@ Widget _buildThirdPartyLogin(String loginType, String logo){
         child: Column(
           children: [
             _buildLogo(),
-            _buildThirdPartyLogin("Google","google"),
-            _buildThirdPartyLogin("Facebook","facebook"),
-            _buildThirdPartyLogin("Apple","apple"),
-            Container(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Divider(
-                      height: 2.h,
-                      color: AppColors.primarySecondaryElementText,
-                    ),
-                  ),
-                  const Expanded(
-                    child: Text(
-                      "or"
-                    ),
-                  ),
-                  Expanded(
-                    child: Divider(
-                      height: 2.h,
-                      color: AppColors.primarySecondaryElementText,
-                    ),
-                  ),
-                ],
-              )
+            _buildThirdPartyLogin("Google", "google"),
+            _buildThirdPartyLogin("Facebook", "facebook"),
+            _buildThirdPartyLogin("Apple", "apple"),
+            _buildOrWidget(),
+            _buildThirdPartyLogin("Phone Number", ""),
+            SizedBox(
+              height: 35.h,
             ),
+            _buildSignUpWidget()
           ],
         ),
-      )
+      ),
     );
   }
 }
